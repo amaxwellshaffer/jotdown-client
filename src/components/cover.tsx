f52532851ceb10886abf49547c645ec266f198a3
Max Shaffer <max@Maxs-MBP.attlocal.net>
2021-07-13 22:04:54 -0400

    second 7-13

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class Cover extends React.Component {
 
@@ -7,6 +9,13 @@ class Cover extends React.Component {
             <div className="cover">
                 <h1>JOTDOWN</h1>
                 <h4>bells whistles distractions</h4>
+                <TextField id="standard" label="username" />
+                <br />
+                <TextField id="standard" label="password" />
+                <br />
+                <br />
+                <Button onClick={() => { alert('clicked') }} variant="outlined">login</Button>
+                <Button onClick={() => { alert('clicked') }} variant="outlined">register</Button>
             </div>
         )
     }
