051c94feba1c71ac0e27d1e01fcb7ba3a183e980
Max Shaffer <max@Maxs-MBP.attlocal.net>
2021-07-20 21:30:16 -0400

    7-20

--- src/components/inside.tsx
+++ src/components/inside.tsx
@@ -1,11 +1,58 @@
 import React from "react";
+import Notepad from "./notepad";
 
-class Inside extends React.Component {
+interface IProps {};
+interface IState {
+    notes: (string | null),
+    checklist: (string | boolean | number)[],
+    journal: (string | boolean | number)[],
+    //reqHeaders: HeadersInit,
+};
+class Inside extends React.Component <IProps, IState>{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            notes:'',
+            checklist:[],
+            journal:[],
+            //reqHeaders: []
+          
+        };
+    }
+
+    serverurl: string = 'http://localhost:3005';
+
+    componentDidMount(){
+
+    const reqHeaders: HeadersInit = {
+            'Content-Type': 'application/json',
+            'Authorization': `${localStorage.getItem("token")}`
+    }
+
+        fetch(`${this.serverurl}/user/`, {
+            method: "GET",
+            headers: reqHeaders,
+          })
+            .then((res) => res.json())
+            .then((json) => {
+                console.log(json);
+                console.log(json.foundUser[0].notepad[0].notes);
+                
+                this.setState({ notes: json.foundUser[0].notepad[0].notes });
+                this.setState({ checklist: json.foundUser[0].checklist });
+                this.setState({ journal: json.foundUser[0].logEntries });
+              console.log(this.state.notes);
+            });
+        };
+    
 
     render(){
         return(
             <div className="inside">
-                <div className="leftPage">Left Page</div>
+                <div className="leftPage">
+                    <Notepad notes={this.state.notes}/>
+                </div>
                 <div className="rightPage"> 
                     <div className="right-top">top</div>
                     <div className="right-bottom">bottom</div>
---
+++ src/components/notepad.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+//import TextField from '@material-ui/core/TextField';
+import { TextField, Button } from "@material-ui/core";
+
+interface IProps { 
+    notes: (string | null ),
+ };
+interface IState { notes: (string | null) };
+
+class Notepad extends React.Component<IProps, IState>{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: ''
+        };
+    }
+
+    serverurl: string = 'http://localhost:3005';
+
+    clickHandle = () => {
+        console.log(this.state.notes);
+        
+        fetch(`${this.serverurl}/notepad/`, {
+            method: 'PUT',
+            body: JSON.stringify({ notes: this.state.notes }),
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `${localStorage.getItem("token")}`
+            })
+        }).then(
+            (response) => response.json()
+        ).then((json) => {
+            console.log(json);
+
+        }).catch((err) => {
+            console.log(err);
+            alert("not saved")
+        })
+    }
+
+
+    // componentDidMount() {
+    //     this.setState({notes: this.props.notes})
+    //     console.log('from notepad mount', this.props.notes); 
+    // }
+
+    render() {
+        
+
+        //console.log(this.props.notes);
+        
+        return (
+            <div className="notepad">
+                <TextField
+                    id="standard-multiline-flexible"
+                    margin="normal"
+                    multiline
+                    rows={26}
+                    //value={this.state.notes}
+                    defaultValue={this.props.notes}
+                    onChange={e => this.setState({ notes: e.target.value })}
+                    fullWidth
+                />
+                <Button onClick={this.clickHandle}>Save</Button>
+            </div>
+        )
+
+    }
+
+}
+
+export default Notepad;
